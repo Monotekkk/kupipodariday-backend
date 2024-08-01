45e3a7bfcfc213feba3c3d588dda435dee31fbcf
Тимур <timur.yakhin.99@yandex.ru>
2024-08-01 22:55:40 +0300

    push wishlists

--- src/wishlists/wishlists.controller.ts
+++ src/wishlists/wishlists.controller.ts
@@ -6,40 +6,50 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { WishlistsService } from './wishlists.service';
 import { CreateWishlistDto } from './dto/create-wishlist.dto';
 import { UpdateWishlistDto } from './dto/update-wishlist.dto';
+import { AuthUser } from 'src/common/decorators/user.decorator';
+import { User } from 'src/users/entities/user.entity';
+import { Wishlist } from './entities/wishlist.entity';
+import { JwtAuthGuard } from 'src/auth/guard/jwt-auth.guard';
 
-@Controller('wishlists')
+@UseGuards(JwtAuthGuard)
+@Controller('wishlistlists')
 export class WishlistsController {
   constructor(private readonly wishlistsService: WishlistsService) {}
 
   @Post()
-  create(@Body() createWishlistDto: CreateWishlistDto) {
-    return this.wishlistsService.create(createWishlistDto);
+  create(
+    @AuthUser() user: User,
+    @Body() dto: CreateWishlistDto,
+  ): Promise<Wishlist> {
+    return this.wishlistsService.create(user, dto);
   }
 
   @Get()
-  findAll() {
+  findAll(): Promise<Wishlist[]> {
     return this.wishlistsService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  findOne(@Param('id') id: string): Promise<Wishlist> {
     return this.wishlistsService.findOne(+id);
   }
 
   @Patch(':id')
   update(
+    @AuthUser() user: User,
     @Param('id') id: string,
-    @Body() updateWishlistDto: UpdateWishlistDto,
+    @Body() dto: UpdateWishlistDto,
   ) {
-    return this.wishlistsService.update(+id, updateWishlistDto);
+    return this.wishlistsService.update(+id, dto, +user.id);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.wishlistsService.remove(+id);
+  remove(@AuthUser() user: User, @Param('id') id: string) {
+    return this.wishlistsService.remove(+id, +user.id);
   }
 }
